3a87b16bd40c1acfb4e3de2afb3bae5471a7eac7
pawel33332 <pawel33332@gmail.com>
2021-11-09 20:48:06 +0100

    wersja 1.0

--- Active_Life/Active_Life/Kompas.xaml.cs
+++ Active_Life/Active_Life/Kompas.xaml.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+using Xamarin.Essentials;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
@@ -12,9 +12,15 @@ namespace Active_Life
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class Kompas : ContentPage
     {
+       
         public Kompas()
         {
             InitializeComponent();
+           
         }
+
+     
+
+
     }
 }
