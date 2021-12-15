007594a016c2f3849c2e4c550b442dfd2fb9ff9e
pawel33332 <92433894+pawel33332@users.noreply.github.com>
2021-12-15 12:07:32 +0100

    dodano zapisywanie i odczytywanie trasy z pliku

--- Active_Life/Active_Life/trasa.xaml.cs
+++ Active_Life/Active_Life/trasa.xaml.cs
@@ -1,54 +1,172 @@
-ï»¿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Threading;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 using Xamarin.Forms.Maps;
 using Plugin.Geolocator;
 using Xamarin.Essentials;
+using System.IO;
+
 
 namespace Active_Life
 {
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class Trasa : ContentPage
     {
-        public int zakoncz_trase = 0;
+        
+        string trasa_file = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData), "path.txt");
+        string trasa_file_stat = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData), "last_path.txt");
+        string wartosc;
+        double[] tab_pozycja = new double[1000];
+     
+
         public Trasa()
         {
             InitializeComponent();
             Trasa_Page.BackgroundColor = Active_Life.Button_Page.tlo_trasy;
-            Sledz_trase(null, EventArgs.Empty);
+            obecna_pozycja();
+            odczytaj_trase();
+
         }
-        async void Sledz_trase(object sender, EventArgs e)
+        async private void Wyswietl_statystyki(object sender, EventArgs e)
         {
-            zakoncz_trase = 0;
-            while(zakoncz_trase==0)
-            {
-            var request = new GeolocationRequest(GeolocationAccuracy.Medium, TimeSpan.FromSeconds(10));
+            await Navigation.PushAsync(new Statystyki_Page());
+
+        }
+        async private void obecna_pozycja()
+        {
+            var request = new GeolocationRequest(GeolocationAccuracy.Medium, TimeSpan.FromSeconds(2));
             var location = await Geolocation.GetLocationAsync(request);
             map.MoveToRegion(MapSpan.FromCenterAndRadius(new Position(location.Latitude, location.Longitude)
-            ,Distance.FromMiles(1)));
-            Polygon polygon = new Polygon
+            , Distance.FromMiles(2)));
+        }
+        async private void Sledz_trase(object sender, EventArgs e)
+        {
+            int a = Preferences.Get("trasa_aktywna", 0);
+            if (a == 0)
             {
-                StrokeWidth = 10,
+                File.WriteAllText(trasa_file, String.Empty);
+                sledzenie();
+                Preferences.Set("trasa_aktywna", 1);
+            } else
+            {
+             await DisplayAlert("Informacja", "Nie mo¿na rozpocz¹æ nowej trasy. Musisz zakoñczyæ obecn¹", "OK");
+            }
+
+        }
+        async private void sledzenie()
+        {
+            
+            var request = new GeolocationRequest(GeolocationAccuracy.Medium, TimeSpan.FromSeconds(2));
+            var location = await Geolocation.GetLocationAsync(request);
+            map.MoveToRegion(MapSpan.FromCenterAndRadius(new Position(location.Latitude, location.Longitude)
+            , Distance.FromMiles(2)));
+
+            using (StreamWriter sw = File.AppendText(trasa_file))
+            {
+                sw.WriteLine(location.Latitude);
+                sw.WriteLine(location.Longitude);
+
+            }
+            using (StreamReader sr = new StreamReader(trasa_file))
+            {
+
+                int i = 0;
+                while ((wartosc = sr.ReadLine()) == null)
+                {
+                    tab_pozycja[0] = location.Latitude;
+                    tab_pozycja[1] = location.Longitude;
+
+                    string[] lines =
+                    {
+                          tab_pozycja[0].ToString(),tab_pozycja[1].ToString()
+                        };
+                    File.WriteAllLines(trasa_file, lines);
+
+                }
+
+            }
+            odczytaj_trase();
+        }
+         async private void odczytaj_trase()
+        {
+
+            Polyline polyline = new Polyline
+            {
+                StrokeWidth = 20,
                 StrokeColor = Color.FromHex("#dd9ecd"),
-                FillColor = Color.FromHex("#800080"),
+
                 Geopath =
             {
-              new Position(Preferences.Get("szerokosc_geo",location.Latitude),Preferences.Get("dlugosc_geo",location.Longitude)),
-              new Position(location.Latitude, location.Longitude)
+
+
             }
             };
-            map.MapElements.Add(polygon);
-            Preferences.Set("szerokosc_geo", location.Latitude);
-            Preferences.Set("dlugosc_geo", location.Longitude);
-                
-            }
-            }
-      
-         
+            using (StreamReader sr = new StreamReader(trasa_file))
+            {
+                int i = 0;
+                while ((wartosc = sr.ReadLine()) != null)
+                {
+                    if (i % 2 == 0)
+                    {
 
-    }  
+                        float lattitude = float.Parse(wartosc);
+
+                        tab_pozycja[i] = lattitude;
+
+                    }
+                    else
+                    {
+                        float longitude = float.Parse(wartosc);
+                        tab_pozycja[i] = longitude;
+                    }
+                    i++;
+                }
+             for (int a = 0; a <i; a+=2)
+            {
+
+                polyline.Geopath.Add(new Position(tab_pozycja[a], tab_pozycja[a+1]));
+
+            }
+            }
+              
+            map.MapElements.Add(polyline);
+            if(Preferences.Get("trasa_aktywna", 0) == 1)
+            {
+            await Task.Delay(30000);
+            sledzenie();
+            }
+            
+        }
+        
+        async private void Koniec_trasy(object sender, EventArgs e)
+        {
+            if(Preferences.Get("trasa_aktywna",0)==1)
+            {
+            using (var reader = new StreamReader(trasa_file))
+            using (var writer = new StreamWriter(trasa_file_stat))
+            {
+                writer.Write(reader.ReadToEnd());
+            }
+            Preferences.Set("trasa_aktywna", 0);
+            File.WriteAllText(trasa_file, "");
+            await Navigation.PushAsync(new Trasa());
+            await DisplayAlert("Informacja", "Zakoñczono trasê. Przejdz do zak³adki statystyki aby zobaczyæ ostatni¹ trasê z zebranymi informacjami.", "OK");
+            
+            } else
+            {
+           await DisplayAlert("Informacja", "Nie mo¿na zakoñczyæ trasy, poniewa¿ nie zosta³a rozpoczêta", "OK");
+            }
+            
+            
+
+        }
+      
+
+
+    }
 }
